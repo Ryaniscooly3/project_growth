b63a17a79f39fcda6c2fec5d1fdfe09428a887d0
jessicasu77 <jessica.su0807@gmail.com>
2021-02-21 07:04:28 -0500

    fix new merge conflicts

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,8 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import VineList from "./VineList";
 import Dropzone from "react-dropzone";
 import request from "superagent";
-<<<<<<< Updated upstream
 import App from "./App";
-=======
-import React from 'react';
->>>>>>> Stashed changes
 
 const Home = () => {
   const [vines, setVines] = useState([]);
