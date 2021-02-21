667ca852bd51e34ca9d6e94bf8d0c929b59a62a8
jessicasu77 <jessica.su0807@gmail.com>
2021-02-20 19:07:18 -0500

    Renders Prompt Component in Home Page

--- src/App.js
+++ src/App.js
@@ -13,3 +13,21 @@ function App() {
 }
 
 export default App;
+
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import Prompt from './components/Prompt'
+
+function App() {
+    const [whatGoal, setGoal] = useState("");
+    return ( <div>
+        {/*  pass setter function down  */}
+        <Prompt whatGoal={whatGoal} setGoal={setGoal} />
+        {/* display goal */}
+        <p>{whatGoal}</p>
+        </div>
+    );
+}
+
+
+ReactDOM.render(<App/>, document.getElementById('root'))
