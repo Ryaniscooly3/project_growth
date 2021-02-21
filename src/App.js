7cdf7d2a2eb1d9fb554301b072297c4b7c7279db
Jamie Lin <jamie.miki.lin@gmail.com>
2021-02-20 22:54:12 -0500

    resolving merge conflicts

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import Home from "./Home";
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import Prompt from "./components/Prompt";
+// import Footer from "./components/footer";
 
 function App() {
   const [Goal, setGoal] = useState("");
   return (
-    <div className="App">
-      <Prompt Goal={Goal} setGoal={setGoal} />
-      <div className="stylegoal">{Goal}</div>
-      <Navbar />
-      <div className="content">
-        <Home />
+      <div className="App">
+        <Prompt Goal={Goal} setGoal={setGoal} />
+        <div className="stylegoal">{Goal}</div>
+        <Navbar />
+        <div className="content">
+          <Home />
+        </div>
+        {/*<Footer/>*/}
       </div>
-    </div>
   );
 }
 export default App;
